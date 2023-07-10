e1840e8c8ef0196df0afae57eb49cd0485e2e3e5
devanshiipandey <136778589+devanshiipandey@users.noreply.github.com>
2023-07-09 21:55:14 -0400

    Create lab_assignment_7.c

---
+++ lab_assignment_7.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void swapCounter(int x){
+    printf("%d ",x);
+}
+
+int main() {
+    
+    int array[] = {97, 16, 45, 63, 13, 22, 7, 58, 72};
+    int n = sizeof(array)/sizeof(array[0]);  
+    int i, j, tmp, swap;
+
+    
+    for (i = 0; i < n; i++) {
+        int count = 0;
+        swap = 0; 
+
+        for (j = 0; j < n-i-1; j++) {
+            
+
+            if (array[j] > array[j+1]) {
+                
+                tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+                
+                swap++;
+                count++;
+            }
+        }
+        swapCounter(count);
+      
+        if (swap == 0) 
+            break;
+    }
+
+    return 0;
+}
